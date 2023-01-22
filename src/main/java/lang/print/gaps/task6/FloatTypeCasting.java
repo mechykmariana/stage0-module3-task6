1f80536dad28edb6070319d80e06bb9232cf82ed
mariana_mechyk <mariana.mechyk@ecenta.com>
2023-01-22 17:24:56 +0200

    Type Casting

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task6;
 
+import static java.lang.Math.round;
+
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        numberToBeRounded = round(numberToBeRounded);
+        System.out.println(numberToBeRounded);
     }
 }
